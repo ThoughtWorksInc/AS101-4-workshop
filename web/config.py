57d444b8ec356a5ffc2f9e4eca2122f7865ed698
Willem van Ketwich <willvk@gmail.com>
2019-12-08 14:57:54 +1100

    initial commit

---
+++ web/config.py
@@ -0,0 +1,14 @@
+import os
+
+
+class BaseConfig(object):
+    SECRET_KEY = "changeme"
+    DEBUG = os.environ['DEBUG']
+    DB_NAME = "postgres"
+    DB_USER = "postgres"
+    DB_PASS = "postgres"
+    DB_SERVICE = "postgres"
+    DB_PORT = "5432" 
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
+        DB_USER, DB_PASS, DB_SERVICE, DB_PORT, DB_NAME
+    )
