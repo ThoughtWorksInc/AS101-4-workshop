eeeb716017fe3a1e4cc1c8a86a731c112f8993b5
Willem van Ketwich <willvk@gmail.com>
2020-04-17 22:52:23 +1000

    move web app startup commands into a script

---
+++ web/start-app.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while ! timeout 1 bash -c 'cat < /dev/null > /dev/tcp/postgres/5432 2>&1 > /dev/null'; do
+    echo "Attempting to connect to postgres db"
+    sleep 1
+done
+
+echo "connected!"
+
+python /usr/src/app/create_db.py
+
+/usr/local/bin/gunicorn -w 2 -b :8000 app:app
